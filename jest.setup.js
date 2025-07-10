cc40f074fb12c5f7feeacb63ddbc8365ded4fa5e
lukluken <lucasmellofx@hotmail.com>
2025-07-10 15:41:03 -0300

    Subindo API de gerador de senha com CI

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,22 @@
 import '@testing-library/jest-dom' 
+
+if (typeof global.TextEncoder === 'undefined') {
+  global.TextEncoder = require('util').TextEncoder;
+}
+if (typeof global.TextDecoder === 'undefined') {
+  global.TextDecoder = require('util').TextDecoder;
+}
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+}); 
